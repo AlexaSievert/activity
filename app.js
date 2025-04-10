d8bc84ca8f4d1d9a259002875aaea549f39f346a
rishikagudise <gudiserishika@gmail.com>
2025-04-10 15:39:08 -0500

    added objects

--- app.js
+++ app.js
@@ -1,9 +1,57 @@
-let rm = {
-  name: "real madrid",
-  city: "madrid",
-  country: "spain",
-  top_scorers: ["ronaldo", "benzema", "hazard"],
-  fans_count: 798,
+console.log(firebase);
+
+let ob1 = {
+  team_name: "Real Madrid",
+  city: "Madrid",
+  country: "Spain",
+  top_scorers: ["Ronaldo", "Benzema", "Hazard"],
+  fan_count: "798",
 };
 
-db.collection("teams").add(rm);
+let ob2 = {
+  team_name: "Barcelona",
+  city: "Barcelona",
+  country: "Spain",
+  top_scorers: ["Messi", "Suarez", "Puyol"],
+  fan_count: "738",
+};
+
+let ob3 = {
+  team_name: "Manchester United",
+  city: "Manchester",
+  country: "England",
+  top_scorers: ["Cantona", "Rooney", "Ronaldo"],
+  fan_count: "755",
+};
+
+let ob4 = {
+  team_name: "Manchester City",
+  city: "Manchester",
+  country: "England",
+  top_scorers: ["Sterling", "Aguero", "Haaland"],
+  fan_count: "537",
+};
+
+let ob5 = {
+  team_name: "Brazil National Team",
+  city: "Not applicable",
+  country: "Brazil",
+  top_scorers: ["Ronaldinho", "Cafu", "Bebeto"],
+  fan_count: "950",
+};
+
+let ob6 = {
+  team_name: "Argentina national team",
+  city: "Not applicable",
+  country: "Argentina",
+  top_scorers: ["Messi", "Batistuta", "Maradona"],
+  fan_count: "888",
+};
+
+let ob7 = {
+  team_name: "Atletico Madrid",
+  city: "Madrid",
+  country: "Spain",
+  top_scorers: ["Aragonés", "Griezmann", "Torez"],
+  fan_count: "400",
+};
